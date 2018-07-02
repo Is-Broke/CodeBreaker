5197790608e7c750dee40437b2e40d857d41d63c
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-02 14:22:54 -0700

    Created database to store comments for error examples

---
+++ SyntacsApp/Migrations/20180702211718_initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SyntacsApp.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Alias = table.Column<string>(maxLength: 20, nullable: false),
+                    CommentBody = table.Column<string>(maxLength: 150, nullable: false),
+                    ErrExampleID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+        }
+    }
+}
---
+++ SyntacsApp/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyntacsApp.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        [StringLength(20, MinimumLength=3),Required]
+        public string Alias { get; set; }
+        [StringLength(150), Required]
+        public string CommentBody { get; set; }
+        public int ErrExampleID { get; set; }
+    }
+}
