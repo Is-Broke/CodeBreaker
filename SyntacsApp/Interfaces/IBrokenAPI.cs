daecc83d9265723a730d8bc57b3ae0dc110c70c2
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-02 18:42:01 -0700

    Added an interface to migrate api calls into

---
+++ SyntacsApp/Interfaces/IBrokenAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyntacsApp.Interfaces
+{
+    //TODO: Put all api calls in here for models and controllers to utilize
+    //and prevent from repeating calls
+    interface IBrokenAPI
+    {
+    }
+}
