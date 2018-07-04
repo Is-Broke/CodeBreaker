7c2ce2819392c738ec6beb82ae3358a9e1c6bc6e
Eric Singleton <eric.singleton.jr@gmail.com>
2018-07-04 14:32:26 -0700

    Updated db connection string

--- SyntacsApp/Startup.cs
+++ SyntacsApp/Startup.cs
@@ -28,7 +28,7 @@ namespace SyntacsApp
         {
             services.AddMvc();
             services.AddDbContext<SyntacsDbContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                    options.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
